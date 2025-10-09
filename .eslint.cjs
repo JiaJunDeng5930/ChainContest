41d263bf6ebd954356cd847273bea7e2dfddb9f0
JiajuDeng <jiajundeng@zju.edu.cn>
2025-10-08 19:00:38 -0700

    chore: 配置共享代码规范

---
+++ .eslint.cjs
@@ -0,0 +1,76 @@
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  ignorePatterns: [
+    "dist",
+    "build",
+    "coverage",
+    "artifacts",
+    "cache",
+    "node_modules",
+  ],
+  env: {
+    es2021: true,
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: [
+          path.join(__dirname, "contracts/tsconfig.json"),
+          path.join(__dirname, "frontend/tsconfig.json"),
+        ],
+        tsconfigRootDir: __dirname,
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      rules: {
+        "@typescript-eslint/no-misused-promises": [
+          "error",
+          {
+            checksVoidReturn: {
+              attributes: false,
+            },
+          },
+        ],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-floating-promises": [
+          "error",
+          {
+            ignoreVoid: true,
+          },
+        ],
+      },
+    },
+    {
+      files: ["frontend/**/*.{ts,tsx}", "frontend/**/*.test.{ts,tsx}"],
+      env: {
+        browser: true,
+      },
+      extends: ["plugin:react-hooks/recommended", "plugin:react/recommended"],
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      rules: {
+        "react/react-in-jsx-scope": "off",
+      },
+    },
+    {
+      files: ["contracts/**/*.ts"],
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
